72cf8fbd42d041808e00c1992f5e00dc90630398
KirillBogdanov1 <144319949+KirillBogdanov1@users.noreply.github.com>
2024-02-21 11:47:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from pyscript import document
+
+
+
+
+
+
+
+
+
+def translate_english(event):
+    input_text = document.querySelector("#zn1")
+
+    zn1 = input_text.value
+
+
+    excel_data_df = pd.read_excel('C:/fakepath/adress1xlsx',index_col=None)
+    dfs = []
+    add111=0
+    k=0
+    for i in excel_data_df['название Адреса'].tolist():
+        print(i)
+
+
+
+
+
+    
+    output_div = document.querySelector("#output")
+    output_div.innerText =zn1 
+
